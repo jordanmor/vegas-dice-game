78ae9335c2932020123fb4f8e688dbb6403de89f
Jordan Mor <jordanmor@gmail.com>
2019-09-07 22:02:03 -0400

    Add view logic that handles changing the bet amount

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,8 +2,8 @@ let host = 'https://vegas-dice-game.herokuapp.com'; // Heroku
 let homePath = '/vegas-dice-game/'; // GitHub Pages
 
 // Local host and path for testing and development
-// host = 'http://localhost:8080';
-// homePath = '/';
+host = 'http://localhost:8080';
+homePath = '/';
 
 const rolls = ['roll-one', 'roll-two', 'roll-three', 'roll-four', 'roll-five', 'roll-six'];
 const dieOne = document.getElementById('dieOne');
@@ -12,6 +12,8 @@ const animationDuration = 2300;
 let playerName = 'Player';
 let currentScore = 0;
 let point = 0;
+let betAmount = 0;
+let changedBetAmount = betAmount;
 
 // Collection of SVG die faces
 const dieFaceSvgs = {
@@ -55,9 +57,27 @@ function init() {
     playerName = gameData.player.name;
     currentScore = gameData.score;
     point = gameData.point;
+    betAmount = gameData.bet;
+    $('#bet').text(betAmount);
+    $('#credits').text(currentScore);
     $('.playerName').text(gameData.player.name);
+    $('#changeBet').hide();
+
+    // If the page refreshes when the player's credits have already gone to 0,
+    // reopen the modal that gives player a choice to leave the game or start a new one.
+    if(gameData.score === 0) {
+
+      setModalInfo('You are out of credits', 'Would you like to play a new game?', 'New Game', 'Leave Game');
+  
+      $('#modal').modal({
+        show: true,
+        keyboard: false,
+        backdrop: 'static'
+      });
+    }
   });
 
+  // Fetch and populate the page with the top ten players scores
   fetch(`${host}/highest-scores`)
     .then(response => response.json())
     .then(players => {
@@ -99,7 +119,7 @@ function populateCubeFaces() {
         ******** EVENT LISTENERS ********
 // ==========-==========-==========-========== */
 
-// Roll the Dice
+// ROLL THE DICE
 $('#roll').on('click', function() {
 
   const previousScore = currentScore;
@@ -109,7 +129,6 @@ $('#roll').on('click', function() {
   fetch(`${host}/game?action=play`)
   .then(response => response.json())
   .then(gameData => {
-
     const regex = /roll-\w+/;
     $(this).css({ 
       "pointer-events":"none",
@@ -145,6 +164,14 @@ $('#roll').on('click', function() {
       // Animate message that alerts player to roll result
       $('#playerMessage').addClass('animate-data-change');
 
+      // On a losing roll, if player credits are less than bet amount, match bet amount to player credits
+      if(gameData.message !== null && gameData.message.includes('You Lost') && betAmount > currentScore) {
+        betAmount = currentScore;
+        // Update server side logic with new bet amount
+        fetch(`${host}/change-bet?bet=${betAmount}`);
+        $('#bet').text(betAmount);
+      }
+
       // Animate score if there is a change
       if(currentScore !== previousScore) {
         $('#credits').addClass('animate-data-change');
@@ -152,7 +179,6 @@ $('#roll').on('click', function() {
 
       // When player is out of credits, show modal for game exit or new game
       if(gameData.score === 0) {
-        console.log("Game Over");
 
         setModalInfo('You are out of credits', 'Would you like to play a new game?', 'New Game', 'Leave Game');
 
@@ -184,16 +210,28 @@ $('#modalBtnOne').on('click', function() {
       });
     });
   }
+  $('#changeBet').hide();
 });
 
-// End game
+// When clicked, modal button two will either... 
+// Set the bet amount or... 
+// End the game
 $('#modalBtnTwo').on('click', function() {
-  fetch(`${host}/game?action=end`)
-  .then(() => {
-    $('body').fadeOut('slow', function() {
-      location.href=homePath;
+  if(this.textContent === 'Change Bet') {
+    $('#changeBet').hide();
+    // Update server side logic with new bet amount
+    fetch(`${host}/change-bet?bet=${changedBetAmount}`);
+    $('#bet').text(changedBetAmount);
+    betAmount = changedBetAmount;
+    // End game
+  } else {
+    fetch(`${host}/game?action=end`)
+    .then(() => {
+      $('body').fadeOut('slow', function() {
+        location.href=homePath;
+      });
     });
-  });
+  }
 });
 
 // Populate and display modal when player clicks the Cash Out button
@@ -206,5 +244,37 @@ $('#cashOut').on('click', function() {
     keyboard: false,
     backdrop: 'static'
   });
+});
 
+// Open Modal with a change bet option
+$('#openBetModal').on('click', function() {
+  const betHighRange = currentScore > 100 ? 100 : currentScore;
+  setModalInfo('Change Bet Amount', `Bet can be from 10 to ${betHighRange} credits`, 'Go Back','Change Bet');
+
+  $('#changeBet').show();
+
+  changedBetAmount = betAmount;
+  $('.amount').text(changedBetAmount);
+
+  $('#modal').modal({
+    show: true,
+    keyboard: false,
+    backdrop: 'static'
+  }); 
+});
+
+// Change Bet Amount
+$('#changeBet').on('click', function(e) {
+  if($(e.target).hasClass('plus')) {
+    // A bet can range from 10 to 100 credits, unless a player has less than 100 credits, 
+    // in which case, a bet can range from 10 credits to the player's total credits
+    if(changedBetAmount < currentScore && changedBetAmount < 100) {
+      changedBetAmount += 10;
+      $('.amount').text(changedBetAmount);
+    }
+  } else if ($(e.target).hasClass('minus'))
+    if(changedBetAmount > 10) {
+      changedBetAmount -= 10;
+      $('.amount').text(changedBetAmount);
+    }
 });
